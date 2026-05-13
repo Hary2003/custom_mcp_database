0b6c5cac65e69c5f1d43e8f50ed7ecd4547d3f07
Hary2003 <harisankarm2003@gmail.com>
2026-05-13 10:44:23 +0530

    added more tools

--- server.py
+++ server.py
@@ -31,5 +31,31 @@ def delete_employee(id: int):
     db.close()
     return "Employee deleted successfully"
 
+
+@mcp.tool()
+def update_employee(id: int, name: str=None, role:str=None):
+    db = session()
+    employee = db.query(Employee).filter(Employee.id == id).first()
+    if not employee:
+        raise Exception("Employee not found")
+    if name:
+        employee.name = name
+    if role:
+        employee.role = role
+    db.commit()
+    db.close()
+    return f"Employee {id} updated successfully"
+
+@mcp.tool()
+def change_id(id: int, new_id: int):
+    db = session()
+    employee = db.query(Employee).filter(Employee.id == id).first()
+    if not employee:
+        return "Employee not found"
+    employee.id = new_id
+    db.commit()
+    db.close()
+    return f"Employee {id} id changed to {new_id}"
+
 if __name__ == "__main__":
     mcp.run()
